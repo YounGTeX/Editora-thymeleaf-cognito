93e3ff9da50fa38464503feaa512efcbccac03a9
YounGTeX <thiagoextremer@gmail.com>
2022-08-04 11:11:57 -0300

    comment desnecessário 2

--- src/main/java/com/editora/demo/controller/HomeController.java
+++ src/main/java/com/editora/demo/controller/HomeController.java
@@ -29,46 +29,5 @@ public class HomeController {
         }
         return "redirect:oauth2/authorization/cognito";
     }
-
-    // @GetMapping("/artigos")
-    // public String listArtigos(Authentication a, Artigo artigo, @RequestParam(required = false) String titulo, Model model) {
-    //     if (a != null) {
-    //         try {
-    //             List<Artigo> listArt = new ArrayList<Artigo>();
-    //             if (titulo == null) {
-    //                 _artigoRepository.findAll().forEach(listArt::add);
-    //               }
-    //             else
-    //                 _artigoRepository.findByKeyword(titulo).forEach(listArt::add);
-    //             if (listArt.isEmpty())
-    //                 return "index.html";
-
-    //             model.addAttribute("artigo", artigo);
-    //             model.addAttribute("artigos", listArt);
-    //             return "inteface.html";
-    //         } catch (Exception e) {
-    //             return "index.html";
-    //         }
-    //     }
-    //     return "index.html";
-    // }
-
-    // @PostMapping("/")    
-    // public String createArtigo(@RequestBody Artigo artigo, BindingResult result)
-    // {   
-    //     if(result.hasFieldErrors() || result.hasErrors()){
-    //         System.out.println("Existem erros em HomeController.createArtigo()");
-    //     }
-
-    //     try {
-    //         Artigo _a = _artigoRepository.save(new Artigo(artigo.getTitulo(), artigo.getResumo(), artigo.getStatus()));
-
-    //         return "redirect:/api/artigos";
-            
-    //     } catch (Exception e) {
-    //         return "redirect:/api/artigos";
-    //     }
-
-    // }
     
 }
